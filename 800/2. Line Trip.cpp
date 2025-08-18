95ade1a7cd710c6ad0967887325de362ade7f0a8
Paritosh <paritoshsharma2020@gmail.com>
2025-08-19 02:09:51 +0530

    Added 2.Line Trip.cpp

---
+++ 800/2. Line Trip.cpp
@@ -0,0 +1,43 @@
+/*
+    Author : Paritosh Sharma    
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+typedef long double ld;
+typedef long long ll;
+typedef vector<ll> vi;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+const int MOD = 1e9 + 7;
+ 
+void solve() {
+    int n, x;
+    cin >> n >> x;
+    int prev = 0, ans = 0;
+    for (int i = 0; i < n; ++i) {
+      int a; cin >> a;
+      ans = max(ans, a - prev);
+      prev = a;
+    }
+    ans = max(ans, 2 * (x - prev));
+    cout << ans << '\n';
+}
+ 
+ 
+int main() {
+ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    //solve();
+    return 0;
+}
+ 
